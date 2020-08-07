3f239d0e59a7e1b09051ad03c6343611d2309f09
Motianhu <motianhu@evergrande.com>
2020-08-07 08:45:01 +0800

    aop lib ok

--- aoplib/src/main/java/com/smona/aoplib/AopDoubleClick.java
+++ aoplib/src/main/java/com/smona/aoplib/AopDoubleClick.java
@@ -4,17 +4,21 @@ import android.util.Log;
 
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 
+@Aspect
 public class AopDoubleClick {
     @Pointcut("execution(* onClick(..))")
+    //@Pointcut("execution(* android.view.View.OnClickListener.onClick(..))")
     public void methodPointcut() {
     }
 
     @Around("methodPointcut()")
-    public void onDoubleClickListener(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+    public void onDoubleClick(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
         Log.e("motianhu", "proceedingJoinPoint: " + proceedingJoinPoint);
         if (!InterceptDoubleClick.isDoubleClick()) {
+            Log.e("motianhu", "not double click: ");
             proceedingJoinPoint.proceed();
         }
     }
